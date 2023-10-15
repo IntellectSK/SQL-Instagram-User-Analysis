6a9b48676a243b86e23c28eb91433f2cc138263a
Shubham Kharade <122349306+IntellectSK@users.noreply.github.com>
2023-10-15 20:46:46 +0530

    Add files via upload

---
+++ Insta Data output.sql
@@ -0,0 +1,56 @@
+-- Instagram User Analytics
+-- Description:
+-- User analysis is the process by which we track how users engage and interact with our digital -- product (software or mobile application)
+-- in an attempt to derive business insights for marketing, product & development teams.
+
+use ig_clone;
+
+describe users; -- for getting to know about the data description.
+
+-- 1. Find the 5 oldest users of the Instagram from the database provided
+select * from users;
+select username, created_at from users order by created_at desc limit 5;
+
+
+
+-- 2. Find the users who have never posted a single photo on Instagram
+select * from photos, users;
+select * from users u left join photos p on p.user_id = u.id where p.image_url is null order by u.username;
+
+
+-- 3.Identify the winner of the contest and provide their details to the team
+select * from likes, photos, users;
+select likes.photo_id, users.username, count(likes.user_id) as nooflikes
+from likes inner join photos on likes.photo_id = photos.id
+inner join users on photos.user_id = users.id group by
+likes.photo_id, users.username order by nooflikes desc;
+
+
+
+-- 4. Identify and suggest the top 5 most commonly used hashtags on the platform
+select * from photo_tags, tags;
+select t.tag_name, 
+count(p.photo_id) as hashtag from photo_tags p inner join tags t on t.id = p.tag_id 
+group by t.tag_name 
+order by hashtag desc;
+
+
+-- 5.What day of the week do most users register on? Provide insights on when to schedule an ad campaign
+select * from users;
+select DATE_FORMAT((created_at), '%W') as dayy, count(username) from users group by 1 order by 2 desc; 
+-- group by 1 means group by DATE_FORMAT((created_at) and Order by '%W'.
+
+-- 6.Provide how many times does average user posts on Instagram. Also, provide the total number of photos on instagram/total number of users
+
+select * from photos, users; 
+with base as(
+select u.id as userid, count(p.id) as photoid from users u left join photos p on p.user_id = u.id group by u.id)
+select sum(photoid) as totalphotos, count(userid) as total_users, sum(photoid)/count(userid) as photoperuser from base;  
+
+﻿
+
+-- 7. Provide data on users (bots) who have liked every single photo on the site (since any normal user would -- not be able to do this).
+select * from users,likes;
+with base as(
+select u.username, count(l.photo_id) as likess from likes l inner join users u on u.id = l.user_id group by u.username)
+select username, likess from base where likess=(select count(*) from photos) order by username;                                                                                                        
